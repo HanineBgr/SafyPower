74b6b588b9ed42fafef24d8aca0fc3032c9024b8
hanine bouguerra <bouguerra.hanine@esprit.tn>
2025-02-16 13:57:13 +0100

    fixing the side bar in the profile screen

--- src/views/auth/profile.tsx
+++ src/views/auth/profile.tsx
@@ -8,40 +8,36 @@ import FeedbackSection from "../../components/cards/Feedback";
 import BillingInfo from "../../components/cards/BillingInfo";
 import PaymentCards from "../../components/cards/paymentCard";
 
-const ProfileScreen: React.FC = () => {
+const ProfileScreen = () => {
   return (
     <div className="flex h-screen bg-[#e0efff]">
       {/* Sidebar */}
       <Sidebar />
-
-      <div className="absolute top-8 left-1000px] w-[1200px]"> 
+      {/* Main Content */}
+      <div className="flex-1 p-8 overflow-auto">
         {/* Top Section: Profile Card and Invoices */}
-        <div className="flex justify-between w-full">
+        <div className="grid grid-cols-3 gap-6">
           {/* Profile Card */}
-          <div className="w-[900px] h-[180px]">
+          <div className="col-span-2 -ml-40"> 
             <ProfileCard />
           </div>
-
           {/* Invoices Card */}
-          <div className="w-[600px] h-[50px]">
+          <div>
             <InvoicesCard />
           </div>
         </div>
-
         {/* Payment Methods Section */}
-        <div className="mt-6 w-full flex justify-between absolute top-[220px]">
+        <div className="mt-6 -ml-[505px]"> {/* Line 30: Adjusted margin-left to extreme left */}
           <PaymentCards />
         </div>
-
-        {/* Billing & Feedback Section */}
-        <div className="mt-6 flex w-full justify-between absolute top-[320px]">
+        {/* Bottom Section: Billing Info and Feedback */}
+        <div className="mt-6 grid grid-cols-3 gap-6">
           {/* Billing Information */}
-          <div className="w-[700px] h-[220px]">
+          <div className="col-span-2">
             <BillingInfo />
           </div>
-
           {/* Feedback Section */}
-          <div className="w-[580px] h-[220px]">
+          <div>
             <FeedbackSection />
           </div>
         </div>
@@ -49,5 +45,4 @@ const ProfileScreen: React.FC = () => {
     </div>
   );
 };
-
 export default ProfileScreen;
